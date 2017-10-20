9f75bfb181c4e6e077a96878c47c3f49f6ba0547
Jimmi H C <jimmiholstchristensen@gmail.com>
2017-10-20 23:40:50 +0200

    Running of the zig compiler on save

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,11 @@
+'use strict';
 import * as vscode from 'vscode';
+import ZigCompilerProvider from './zigCompilerProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+    let compiler = new ZigCompilerProvider();
+    compiler.activate(context.subscriptions);
+    vscode.languages.registerCodeActionsProvider('zig', compiler);
 }
 
 export function deactivate() {
