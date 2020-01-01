e11c507f59bacd0a4ebb7302f63581345379e310
Colin Svingen <swoogan@gmail.com>
2019-12-31 19:22:48 -0500

    Switches to childProcess execFile from spawn

--- src/zigBuild.ts
+++ src/zigBuild.ts
@@ -30,41 +30,36 @@ export function zigBuild(): void {
     // TODO: switch rootpath to support multi root
     const cwd = vscode.workspace.rootPath;
     const buildPath = config.get<string>("zigPath") || 'zig';
-    let childProcess = cp.spawn(buildPath, processArg, { cwd });
 
     logChannel.appendLine(`Starting building the current workspace at ${cwd}`);
 
-    let decoded = ''
-    if (childProcess.pid) {
-        childProcess.stderr.on('data', (data: Buffer) => {
-            decoded += data;
-        });
-        childProcess.stderr.on('end', () => {
-            logChannel.appendLine(decoded);
-            var diagnostics: { [id: string]: vscode.Diagnostic[]; } = {};
-            let regex = /(\S.*):(\d*):(\d*): ([^:]*): (.*)/g;
+    let childProcess = cp.execFile(buildPath, processArg, { cwd }, (err, stdout, stderr) => {
+        logChannel.appendLine(stderr);
+        var diagnostics: { [id: string]: vscode.Diagnostic[]; } = {};
+        let regex = /(\S.*):(\d*):(\d*): ([^:]*): (.*)/g;
 
-            buildDiagnosticCollection.clear();
-            for (let match = regex.exec(decoded); match;
-                match = regex.exec(decoded)) {
-                let path = match[1].trim();
-                let line = parseInt(match[2]) - 1;
-                let column = parseInt(match[3]) - 1;
-                let type = match[4];
-                let message = match[5];
+        buildDiagnosticCollection.clear();
+        for (let match = regex.exec(stderr); match;
+            match = regex.exec(stderr)) {
+            let path = match[1].trim();
+            let line = parseInt(match[2]) - 1;
+            let column = parseInt(match[3]) - 1;
+            let type = match[4];
+            let message = match[5];
 
-                let severity = type.trim().toLowerCase() === "error" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Information;
-                let range = new vscode.Range(line, column,
-                    line, column + 1);
+            let severity = type.trim().toLowerCase() === "error" ?
+                vscode.DiagnosticSeverity.Error :
+                vscode.DiagnosticSeverity.Information;
 
-                if (diagnostics[path] == null) diagnostics[path] = [];
-                diagnostics[path].push(new vscode.Diagnostic(range, message, severity));
-            }
+            let range = new vscode.Range(line, column, line, column + 1);
 
-            for (let path in diagnostics) {
-                let diagnostic = diagnostics[path];
-                buildDiagnosticCollection.set(vscode.Uri.file(path), diagnostic);
-            }
-        });
-    }
+            if (diagnostics[path] == null) diagnostics[path] = [];
+            diagnostics[path].push(new vscode.Diagnostic(range, message, severity));
+        }
+
+        for (let path in diagnostics) {
+            let diagnostic = diagnostics[path];
+            buildDiagnosticCollection.set(vscode.Uri.file(path), diagnostic);
+        }
+    });
 }
