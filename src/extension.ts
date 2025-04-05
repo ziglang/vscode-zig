ade7f52628a5bf2f17533a7e8a107f7ccaa38c83
Harry Pierson <harry.pierson@dbos.dev>
2025-04-05 13:10:54 -0700

    add toggleMultilineStringLiteral command

--- src/extension.ts
+++ src/extension.ts
@@ -23,12 +23,44 @@ export async function activate(context: vscode.ExtensionContext) {
                 { language: "zig", scheme: "file" },
                 new ZigMainCodeLensProvider(),
             ),
+            vscode.commands.registerCommand(
+                'zig.toggleMultilineStringLiteral', 
+                toggleMultilineStringLiteral
+            ),
         );
-
         void activateZls(context);
     });
 }
 
 export async function deactivate() {
     await deactivateZls();
+}
+
+async function toggleMultilineStringLiteral() {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) { return; }
+	const { document, selection } = editor;
+	if (document.languageId !== 'zig') { return; }
+
+	let newText = '';
+	let range = new vscode.Range(selection.start, selection.end);
+
+	const firstLine = document.lineAt(selection.start.line);
+	const nonWhitespaceIndex = firstLine.firstNonWhitespaceCharacterIndex;
+
+	for (var lineNum = selection.start.line; lineNum <= selection.end.line; lineNum++) {
+		const line = document.lineAt(lineNum);
+
+		const isMLSL = line.text.slice(line.firstNonWhitespaceCharacterIndex).startsWith('\\\\');
+		const breakpoint = Math.min(nonWhitespaceIndex, line.firstNonWhitespaceCharacterIndex);
+
+		const newLine = isMLSL
+			? line.text.slice(0, line.firstNonWhitespaceCharacterIndex) + line.text.slice(line.firstNonWhitespaceCharacterIndex).slice(2)
+			: line.text.slice(0, breakpoint) + '\\\\' + line.text.slice(breakpoint);
+		newText += newLine;
+		if (lineNum < selection.end.line) { newText += '\n'; }
+		range = range.union(line.range);
+	}
+
+	await editor.edit((builder) => builder.replace(range, newText));
 }
