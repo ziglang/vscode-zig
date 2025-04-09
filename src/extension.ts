9f350f6c1b8225c0ff087452b8700d0cd0fc0510
Veikka Tuominen <git@vexu.eu>
2025-04-09 20:17:09 +0300

    run format

--- src/extension.ts
+++ src/extension.ts
@@ -23,10 +23,7 @@ export async function activate(context: vscode.ExtensionContext) {
                 { language: "zig", scheme: "file" },
                 new ZigMainCodeLensProvider(),
             ),
-            vscode.commands.registerCommand(
-                'zig.toggleMultilineStringLiteral',
-                toggleMultilineStringLiteral
-            ),
+            vscode.commands.registerCommand("zig.toggleMultilineStringLiteral", toggleMultilineStringLiteral),
         );
         void activateZls(context);
     });
@@ -38,11 +35,11 @@ export async function deactivate() {
 
 async function toggleMultilineStringLiteral() {
     const editor = vscode.window.activeTextEditor;
-    if (!editor) { return; }
+    if (!editor) return;
     const { document, selection } = editor;
-    if (document.languageId !== 'zig') { return; }
+    if (document.languageId !== "zig") return;
 
-    let newText = '';
+    let newText = "";
     let range = new vscode.Range(selection.start, selection.end);
 
     const firstLine = document.lineAt(selection.start.line);
@@ -51,18 +48,22 @@ async function toggleMultilineStringLiteral() {
     for (let lineNum = selection.start.line; lineNum <= selection.end.line; lineNum++) {
         const line = document.lineAt(lineNum);
 
-        const isMLSL = line.text.slice(line.firstNonWhitespaceCharacterIndex).startsWith('\\\\');
+        const isMLSL = line.text.slice(line.firstNonWhitespaceCharacterIndex).startsWith("\\\\");
         const breakpoint = Math.min(nonWhitespaceIndex, line.firstNonWhitespaceCharacterIndex);
 
         const newLine = isMLSL
-            ? line.text.slice(0, line.firstNonWhitespaceCharacterIndex) + line.text.slice(line.firstNonWhitespaceCharacterIndex).slice(2)
+            ? line.text.slice(0, line.firstNonWhitespaceCharacterIndex) +
+              line.text.slice(line.firstNonWhitespaceCharacterIndex).slice(2)
             : line.isEmptyOrWhitespace
-                ? ' '.repeat(nonWhitespaceIndex) + '\\\\'
-                : line.text.slice(0, breakpoint) + '\\\\' + line.text.slice(breakpoint);
+              ? " ".repeat(nonWhitespaceIndex) + "\\\\"
+              : line.text.slice(0, breakpoint) + "\\\\" + line.text.slice(breakpoint);
         newText += newLine;
-        if (lineNum < selection.end.line) { newText += '\n'; }
+        if (lineNum < selection.end.line) newText += "\n";
+
         range = range.union(line.range);
     }
 
-    await editor.edit((builder) => { builder.replace(range, newText); });
+    await editor.edit((builder) => {
+        builder.replace(range, newText);
+    });
 }
